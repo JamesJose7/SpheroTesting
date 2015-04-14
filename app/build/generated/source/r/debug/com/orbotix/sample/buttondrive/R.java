dfdd3f36e0e97ba1b668fa045ef766784966ed92
Jose <pepe.96.jose@gmail.com>
2015-04-14 17:12:32 -0500

    Added circle, triangle, square and testing path move paterns

--- app/build/generated/source/r/debug/com/orbotix/sample/buttondrive/R.java
+++ app/build/generated/source/r/debug/com/orbotix/sample/buttondrive/R.java
@@ -12,10 +12,13 @@ public final class R {
     }
     public static final class id {
         public static final int button=0x7f050006;
+        public static final int circle_button=0x7f050007;
         public static final int ninety_button=0x7f050002;
         public static final int one_eighty_button=0x7f050003;
+        public static final int pathing=0x7f050008;
         public static final int sphero_connection_view=0x7f050005;
         public static final int stop_button=0x7f050000;
+        public static final int triangle=0x7f050009;
         public static final int two_seventy_button=0x7f050004;
         public static final int zero_button=0x7f050001;
     }
--- app/src/main/java/com/orbotix/sample/buttondrive/ButtonDriveActivity.java
+++ app/src/main/java/com/orbotix/sample/buttondrive/ButtonDriveActivity.java
@@ -3,13 +3,12 @@ package com.orbotix.sample.buttondrive;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.os.Handler;
+import android.widget.Button;
 
-import java.util.logging.LogRecord;
+import java.util.Random;
 
 import orbotix.robot.base.Robot;
 import orbotix.robot.base.RobotProvider;
-import orbotix.robot.base.RollCommand;
 import orbotix.sphero.ConnectionListener;
 import orbotix.sphero.Sphero;
 import orbotix.view.connection.SpheroConnectionView;
@@ -17,17 +16,22 @@ import orbotix.view.connection.SpheroConnectionView;
 /** Activity for controlling the Sphero with five control buttons. */
 public class ButtonDriveActivity extends Activity {
 
+    public boolean isRolling;
+
     private Sphero mRobot;
 
     /** The Sphero Connection View */
     private SpheroConnectionView mSpheroConnectionView;
 
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+
+
+        /** Sphero **/
         mSpheroConnectionView = (SpheroConnectionView) findViewById(R.id.sphero_connection_view);
         mSpheroConnectionView.addConnectionListener(new ConnectionListener() {
 
@@ -89,36 +93,60 @@ public class ButtonDriveActivity extends Activity {
      */
     public void onControlClick(View v) {
         // Find the heading, based on which button was clicked
-        final float heading;
+        //final float heading;
         switch (v.getId()) {
 
             case R.id.ninety_button:
-                heading = 90f;
+                //heading = 90f;
                 mRobot.setColor(255, 0, 0);
                 break;
 
             case R.id.one_eighty_button:
-                heading = 180f;
+                //heading = 180f;
                 mRobot.setColor(0, 255, 0);
                 break;
 
             case R.id.two_seventy_button:
-                heading = 270f;
+                //heading = 270f;
                 mRobot.setColor(0, 0, 255);
                 break;
 
             case R.id.button:
-                heading = 0f;
+                //heading = 0f;
                 try {
-                    makeASquare();
+                    pathSquare();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.circle_button:
+                try {
+                    pathCircle();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.pathing:
+                try {
+                    pathTest();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.triangle:
+                try {
+                    pathTriangle();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 break;
 
             default:
-                heading = 0f;
-                mRobot.setColor(240, 15, 219);
+               // heading = 0f;
+               // mRobot.setColor(240, 15, 219);
                 break;
         }
 
@@ -129,26 +157,53 @@ public class ButtonDriveActivity extends Activity {
         //mRobot.drive(heading, speed);
     }
 
+    public void pathTriangle() throws InterruptedException {
+        float[] angles = {0f, 120f, 240f};
 
+        for (int i = 0; i < angles.length; i++) {
+            mRobot.drive(angles[i], 1f);
+            mRobot.stop();
+            Thread.sleep(1000);
+        }
+    }
 
-    private void makeASquare() throws InterruptedException {
-
+    private void pathTest() throws InterruptedException {
         mRobot.drive(0f, 1f);
+        Thread.sleep(2000);
         mRobot.stop();
-        Thread.sleep(1000);
-        mRobot.drive(90f, 1f);
-        mRobot.stop();
-        Thread.sleep(1000);
-        mRobot.drive(180f, 1f);
-        mRobot.stop();
-        Thread.sleep(1000);
-        mRobot.drive(270f, 1f);
-        mRobot.stop();
+    }
 
-        /*final Handler handler = new Handler();
+    private void pathCircle() throws InterruptedException {
+
+        for (float i = 0f; i <= 360f; i += 10) {
+            mRobot.drive(i, 1f);
+            mRobot.setColor((int) i, (int) i, (int) i);
+            Thread.sleep(100);
+        }
+
+        mRobot.stop();
+        mRobot.stop();
+        mRobot.stop();
+    }
+
+
+    public void pathSquare() throws InterruptedException {
+
+        float[] angles = {0f, 90f, 180f, 270f};
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(255);
+
+        for (int i = 0; i < angles.length; i++) {
+            mRobot.setColor(randomNumber, randomNumber, randomNumber);
+            mRobot.drive(angles[i], 1f);
+            mRobot.stop();
+            Thread.sleep(1000);
+        }
+
+        /*Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
-                makeASquare(counter);
+                pathSquare(counter);
                 counter =+ 90f;
             }
         }, 3000);*/
