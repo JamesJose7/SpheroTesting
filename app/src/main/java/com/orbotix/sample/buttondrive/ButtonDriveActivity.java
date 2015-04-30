67a70e3721a71d8798a2d316cebd85b4ed1e3136
Jose <pepe.96.jose@gmail.com>
2015-04-30 18:28:57 -0500

    Improve performance

--- app/src/main/java/com/orbotix/sample/buttondrive/ButtonDriveActivity.java
+++ app/src/main/java/com/orbotix/sample/buttondrive/ButtonDriveActivity.java
@@ -2,6 +2,7 @@ package com.orbotix.sample.buttondrive;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +18,8 @@ import orbotix.view.connection.SpheroConnectionView;
 public class ButtonDriveActivity extends Activity {
 
     private Sphero mRobot;
+
+    private Handler mHandler = new Handler();
 
     private float mSpeed = 0;
 
@@ -87,15 +90,15 @@ public class ButtonDriveActivity extends Activity {
     public void pathTriangle() throws InterruptedException {
         float[] angles = {0f, 120f, 240f};
 
-        for (int i = 0; i < angles.length; i++) {
-            mRobot.drive(angles[i], 1f);
+        for (float angle : angles) {
+            mRobot.drive(angle, mSpeed);
             mRobot.stop();
             Thread.sleep(2000);
         }
     }
 
     private void pathTest() throws InterruptedException {
-        mRobot.drive(0f, 1f);
+        mRobot.drive(0f, mSpeed);
         Thread.sleep(2000);
         mRobot.stop();
     }
@@ -107,30 +110,32 @@ public class ButtonDriveActivity extends Activity {
             mRobot.setColor((int) i, (int) i, (int) i);
             Thread.sleep(50);
         }
-        for (float i = 0f; i <= 360f; i += 10) {
-            mRobot.drive(i, 1f);
-            mRobot.setColor((int) i, (int) i, (int) i);
-            Thread.sleep(50);
-        }
-
-        mRobot.stop();
-        mRobot.stop();
         mRobot.stop();
     }
 
 
     public void pathSquare() throws InterruptedException {
 
-        float[] angles = {0f, 90f, 180f, 270f};
+        final float[] angles = {0f, 90f, 180f, 270f};
         Random randomGenerator = new Random();
-        int randomNumber = randomGenerator.nextInt(255);
+        final int randomNumber = randomGenerator.nextInt(255);
 
-        for (int i = 0; i < angles.length; i++) {
-            mRobot.setColor(randomNumber, randomNumber, randomNumber);
-            mRobot.drive(angles[i], 1f);
-            mRobot.stop();
-            Thread.sleep(1000);
-        }
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (float angle : angles) {
+                    mRobot.setColor(randomNumber, randomNumber, randomNumber);
+                    mRobot.drive(angle, mSpeed);
+                    mRobot.stop();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
     }
 
     /**
@@ -138,57 +143,25 @@ public class ButtonDriveActivity extends Activity {
      *
      * @param v The View that had been clicked
      */
-    public void onControlClick(View v) {
+    public void onControlClick(View v) throws InterruptedException {
         // Find the heading, based on which button was clicked
         //final float heading;
         switch (v.getId()) {
 
-            case R.id.ninety_button:
-                //heading = 90f;
-                mRobot.setColor(255, 0, 0);
+            case R.id.squareButton:
+                pathSquare();
                 break;
 
-            case R.id.one_eighty_button:
-                //heading = 180f;
-                mRobot.setColor(0, 255, 0);
-                break;
-
-            case R.id.two_seventy_button:
-                //heading = 270f;
-                mRobot.setColor(0, 0, 255);
-                break;
-
-            case R.id.button:
-                //heading = 0f;
-                try {
-                    pathSquare();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                break;
-
-            case R.id.circle_button:
-                try {
-                    pathCircle();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            case R.id.circleButton:
+                pathCircle();
                 break;
 
             case R.id.pathing:
-                try {
-                    pathTest();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                pathTest();
                 break;
 
             case R.id.triangle:
-                try {
-                    pathTriangle();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                pathTriangle();
                 break;
 
             default:
